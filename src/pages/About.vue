48b3f530547058d40f56461080c69b3cdcb839e7
Danshima <danning.cc@gmail.com>
2019-06-20 00:28:22 +0200

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,16 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>About me</h1>
+    <p>Android developer with a background in education and literature.
+      Born in China, grew to full-size in The Netherlands, and now based in Stockholm, Sweden.
+      Obsessed with food, art, programming, otaku culture, and game(development).</p>
+    <h1>Tech skills and languages</h1>
+      <ul>
+        <li>Build android applications with Java and Kotlin</li>
+        <li>Build frontend web applications with Angular.js and Vue.js</li>
+        <li>Build simple games with Unity and LibGDX</li>
+        <li>Experience with: Java, Kotlin, C#, C++, Python, JS/HTML/CSS</li>
+      </ul>
   </Layout>
 </template>
 
