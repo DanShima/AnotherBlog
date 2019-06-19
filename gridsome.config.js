b68db2fbfe20e4b95e2561b89754eabe57131c35
Danshima <danning.cc@gmail.com>
2019-06-19 23:16:27 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,23 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Just another blog',
+  siteDescription: "A safe space to stalk me",
+  icon: "src/favicon.png",
+  transformers: {
+    remark: {}
+  },
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "blog/**/*.md",
+        typeName: "Post" // the GraphQL entity that will be created
+      }
+    }
+  ]
 }
