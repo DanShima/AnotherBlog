9cfd16b1f697e0b2644623a598d9b546836e07e9
Danshima <danning.cc@gmail.com>
2019-07-04 23:43:57 +0200

    add links to games

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,14 +4,20 @@
     <p>Android developer with a background in education and literature.
       Born in China, grew to full-size in The Netherlands, and now based in Stockholm, Sweden.
       Obsessed with food, art, programming, otaku culture, and game(development).</p>
-    <h1>Tech skills and languages</h1>
+    <h2>Tech skills and languages</h2>
       <ul>
         <li>Build android applications with Java and Kotlin</li>
         <li>Build frontend web applications with Angular.js and Vue.js</li>
         <li>Build simple games with Unity and LibGDX</li>
         <li>Experience with: Java, Kotlin, C#, C++, Python, JS/HTML/CSS</li>
       </ul>
+    <h2>Games</h2>
+    <ul>
+      <li><a href="https://danshima.com/" target="_blank" rel="noopener">HookaGnome</a></li>
+      <li><a href="https://github.com/DanShima/GameProject" target="_blank" rel="noopener">Gear Up</a></li>
+    </ul>
       <Card/>
+
   </Layout>
 </template>
 
