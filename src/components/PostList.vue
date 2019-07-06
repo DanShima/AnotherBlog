681dd7043a44fe59403605c89284c01309e9f88d
Danshima <danning.cc@gmail.com>
2019-07-06 11:06:00 +0200

    added animated arrows to "read more btn"

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,8 +4,14 @@
     <p class="date" v-html="post.date" />
     <h1 class="title" v-html="post.title" />
     <p class="description" v-html="post.description" />
-    <b> {{post.timeToRead}} min read </b> &nbsp;
-    <g-link :to="post.path" class="read">Read more...</g-link>
+    <b class="timeToRead"> {{post.timeToRead}} min read </b>  &nbsp;
+    <g-link :to="post.path" class="read">Read more
+      <i class="arrow">
+      <font-awesome :icon="['fa', 'chevron-right']" size="xs"/>
+      <font-awesome :icon="['fa', 'chevron-right']" size="xs"/>
+      <font-awesome :icon="['fa', 'chevron-right']" size="xs"/>
+      </i>
+    </g-link>
   </div>
 </template>
 
@@ -17,14 +23,58 @@
 .date {
   font-weight: 300;
 }
-.read {
-  padding: 7px;
-  color: #333;
-  text-decoration: none;
-  background: #86A3C3;
-  border-radius: 6%;
-  box-shadow: 0 2px 2px rgba(0,0,0,0.19), 0 2px 2px rgba(0,0,0,0.23);
+
+.timeToRead {
+  color: #86A3C5;
 }
+.read {
+  --text-color: #86A3C3;
+  color: var(--text-color);
+  text-decoration: none;
+  border-radius: 2%;
+  box-shadow: 0 1px 1px var(--text-color), 0 1px 1px var(--text-color);
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  padding: 8px 10px 8px 10px;
+  border: 1px solid var(--text-color);
+	transform: translate(-50%, -50%);
+}
+
+i {
+		animation: arrow-animation 2s;
+		animation-iteration-count: infinite;
+	}
+i:first-child{
+		animation-delay: 0.2s;
+		margin-left: 5px;
+		opacity: 0.15;
+	}
+i + i {
+		animation-delay: 0.4s;
+		opacity: 0.5;
+	}
+i + i + i {
+		animation-delay: 0.6s;
+		opacity: 1;
+	}
+
+.read:hover {
+    cursor: pointer;
+    color: white;
+    background-color: var(--text-color);
+  }
+
+i:hover {
+    color: white;
+}
+
+@keyframes arrow-animation {
+  0%   { opacity: 0.1; }
+  50%  { opacity: 0.5; }
+  100% { opacity: 1; }
+}
+
+
 </style>
 
 <script>
